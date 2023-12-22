3e22101eb211bc462a3aa30436d9c920c933b17b
saiprasanna <saiiprasanna12@gmail.com>
2023-12-22 17:21:30 +0530

    change1

--- array.sh
+++ array.sh
@@ -6,4 +6,4 @@ echo "first values is : ${FRUITS[0]}"
 echo "second value is : ${FRUITS[1]}"
 echo "Third value is : ${FRUITS[2]}"
 
-echo"all fruits : ${FRUITS[@]}"
+echoc "all fruits : ${FRUITS[@]}"

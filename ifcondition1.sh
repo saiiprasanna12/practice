f10dbd0d8445c411f01749655d91a6c225560443
saiprasanna <saiiprasanna12@gmail.com>
2023-12-26 12:11:35 +0530

    m1

--- ifcondition1.sh
+++ ifcondition1.sh
@@ -4,7 +4,7 @@ NUMBER=$1
 
 if [ $UNUMBER -gt 100 ]
 then
-echo "give no $NUMBER IS GREATER THAN 100"
+echo "give number $NUMBER is greater than 100"
 else
 echo "given number $NUMBER is not greater than 100"
 fi 
